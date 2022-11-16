b5f4476b8831159b18bf1b3b07cd872a80e8dfb4
Manuel Espinoza <manueltfk18@gmail.com>
2022-11-16 18:53:12 -0300

    Proyecto actualizado

---
+++ Entrega2 NewHelpNotary/app/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import contacto,Profile,Cliente,ReservaDeHora,Order
+
+# Register your models here.
+
+
+
+admin.site.register(contacto)
+admin.site.register(Profile)
+admin.site.register(Cliente)
+admin.site.register(ReservaDeHora)
+admin.site.register(Order)
+
+
+
